9a646ecdecea87b8bd4f67e9647673c890288df2
Damjan Vujnovic <damjan@samuraiprinciple.com>
2013-03-10 11:08:33 +0000

    added console.log to timer exercises

--- koans/functions-2/spec/1-closure.js
+++ koans/functions-2/spec/1-closure.js
@@ -6,35 +6,49 @@ describe('Warmup - timers and asynchronous specs', function () {
 		you may wish to do that when you move onto the second part of the assignement (because this is a long running suite)
 	*/
 	it('1 - should understand timers', function () {
-		var i = 0;
+		var i = 0, t0 = Date.now();
 		setTimeout(function () {
 			i = 1;
+			console.log(7, i, Date.now() - t0);
 		}, 100);
+		console.log(1, i, Date.now() - t0);
 		waits(90);
+		console.log(2, i, Date.now() - t0);
 		runs(function () {
 			expect(i).toBe(__);
+			console.log(6, i, Date.now() - t0);
 		});
+		console.log(3, i, Date.now() - t0);
 		waits(20);
+		console.log(4, i, Date.now() - t0);
 		runs(function () {
 			expect(i).toBe(__);
+			console.log(8, i, Date.now() - t0);
 		});
+		console.log(5, i, Date.now() - t0);
 		expect(i).toBe(__);
 	});
 	it('2 - should understand timers', function () {
-		var i = 0;
+		var i = 0, t0 = Date.now();
 		expect(i).toBe(__);
+		console.log(1, i, Date.now() - t0);
 		setTimeout(function () {
 			i = 1;
+			console.log(5, i, Date.now() - t0);
 		}, 0);
+		console.log(2, i, Date.now() - t0);
 		expect(i).toBe(__);
 		waits(1);
+		console.log(3, i, Date.now() - t0);
 		runs(function () {
 			expect(i).toBe(__);
+			console.log(6, i, Date.now() - t0);
 		});
+		console.log(4, i, Date.now() - t0);
 		expect(i).toBe(__);
 	});
 	it('3 - should understand timers', function () {
-		var i = 0, loopDueTime = new Date().getTime() + 600;
+		var i = 0, loopDueTime = Date.now() + 1000;
 		setTimeout(function () {
 			i = 1;
 		}, 300);

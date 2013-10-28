107797573d43a8ce75c655f5003f56107b055197
Damjan Vujnovic <damjan@samuraiprinciple.com>
2013-10-28 09:06:46 +0000

    minor updates/fixes

--- koans/observable/spec/eventDispatcherTest06.js
+++ koans/observable/spec/eventDispatcherTest06.js
@@ -1,7 +1,7 @@
 /*global beforeEach, describe, expect, it, jasmine, SAMURAIPRINCIPLE */
 describe('eventDispatcher', function () {
 	'use strict';
-	it('should invoke all listeners, even if one of them throws an error', function () {
+	it('should invoke all listeners even if one of them throws an error', function () {
 		var underTest = SAMURAIPRINCIPLE.eventDispatcher({}),
 			badListener = jasmine.createSpy().andThrow('Error!'),
 			goodListener = jasmine.createSpy();

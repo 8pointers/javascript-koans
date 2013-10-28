826b369a6b48c9913a50d88a8943c34d4d62e2e5
Damjan Vujnovic <damjan@samuraiprinciple.com>
2013-10-28 09:08:44 +0000

    fixed an error in the spec

--- koans/observable/spec/eventDispatcherTest08.js
+++ koans/observable/spec/eventDispatcherTest08.js
@@ -1,8 +1,7 @@
 /*global beforeEach, describe, expect, it, jasmine, SAMURAIPRINCIPLE */
 describe('eventDispatcher', function () {
 	'use strict';
-	it('should be able to cancel event propagation
-		by returning false from event listener', function () {
+	it('should be able to cancel event propagation by returning false from event listener', function () {
 		var underTest = SAMURAIPRINCIPLE.eventDispatcher({}),
 			firstListener = jasmine.createSpy().andReturn(false),
 			secondListener = jasmine.createSpy();

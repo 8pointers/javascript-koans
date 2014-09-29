2b24dda83afb9db47c6223fb9362b7dfd62f2353
Damjan Vujnovic <damjan@samuraiprinciple.com>
2014-09-29 10:47:58 +0100

    minor update

--- koans/objects/spec/9-game-of-life.js
+++ koans/objects/spec/9-game-of-life.js
@@ -2,24 +2,24 @@
 describe('Game of Life', function () {
 	describe('Determining whether cell is alive in next generation', function () {
 		it('should return false when a live cell has fewer than two live neighbours - under-population', function () {
-			expect(false).toBe(SAMURAIPRINCIPLE.isCellAliveInNextGeneration(true, 0));
-			expect(false).toBe(SAMURAIPRINCIPLE.isCellAliveInNextGeneration(true, 1));
+			expect(SAMURAIPRINCIPLE.isCellAliveInNextGeneration(true, 0)).toBe(false);
+			expect(SAMURAIPRINCIPLE.isCellAliveInNextGeneration(true, 1)).toBe(false);
 		});
 		it('should return true when a live cell has two or three live neighbours - survival', function () {
-			expect(true).toBe(SAMURAIPRINCIPLE.isCellAliveInNextGeneration(true, 2));
-			expect(true).toBe(SAMURAIPRINCIPLE.isCellAliveInNextGeneration(true, 3));
+			expect(SAMURAIPRINCIPLE.isCellAliveInNextGeneration(true, 2)).toBe(true);
+			expect(SAMURAIPRINCIPLE.isCellAliveInNextGeneration(true, 3)).toBe(true);
 		});
 		it('should return false when a live cell has more than three live neighbours - overcrowding', function () {
-			expect(false).toBe(SAMURAIPRINCIPLE.isCellAliveInNextGeneration(true, 4));
-			expect(false).toBe(SAMURAIPRINCIPLE.isCellAliveInNextGeneration(true, 5));
-			expect(false).toBe(SAMURAIPRINCIPLE.isCellAliveInNextGeneration(true, 6));
-			expect(false).toBe(SAMURAIPRINCIPLE.isCellAliveInNextGeneration(true, 7));
-			expect(false).toBe(SAMURAIPRINCIPLE.isCellAliveInNextGeneration(true, 8));
+			expect(SAMURAIPRINCIPLE.isCellAliveInNextGeneration(true, 4)).toBe(false);
+			expect(SAMURAIPRINCIPLE.isCellAliveInNextGeneration(true, 5)).toBe(false);
+			expect(SAMURAIPRINCIPLE.isCellAliveInNextGeneration(true, 6)).toBe(false);
+			expect(SAMURAIPRINCIPLE.isCellAliveInNextGeneration(true, 7)).toBe(false);
+			expect(SAMURAIPRINCIPLE.isCellAliveInNextGeneration(true, 8)).toBe(false);
 		});
 		it('should return true when a dead cell has exactly three live neighbours - reproduction', function () {
-			expect(true).toBe(SAMURAIPRINCIPLE.isCellAliveInNextGeneration(false, 3));
-			expect(false).toBe(SAMURAIPRINCIPLE.isCellAliveInNextGeneration(false, 2));
-			expect(false).toBe(SAMURAIPRINCIPLE.isCellAliveInNextGeneration(false, 4));
+			expect(SAMURAIPRINCIPLE.isCellAliveInNextGeneration(false, 3)).toBe(true);
+			expect(SAMURAIPRINCIPLE.isCellAliveInNextGeneration(false, 2)).toBe(false);
+			expect(SAMURAIPRINCIPLE.isCellAliveInNextGeneration(false, 4)).toBe(false);
 		});
 	});
 });

65dc8b4938d69fee184978ff74fe54791b4ddf0d
Damjan Vujnovic <damjan@samuraiprinciple.com>
2017-03-07 09:08:39 +0000

    cleanup

--- koans/objects/spec/4-updating-property-value.js
+++ koans/objects/spec/4-updating-property-value.js
@@ -1,7 +1,7 @@
 describe('Updating property value', function () {
-  var samurai;
+  var samurai1;
   beforeEach(function () {
-    samurai = {
+    samurai1 = {
       name: 'Myamoto',
       address: {
         street: 'Samurai Way',
@@ -11,22 +11,22 @@ describe('Updating property value', function () {
   });
   it('1 - should know how to update primitive values', function () {
     var samurai2 = {
-      name: samurai.name
+      name: samurai1.name
     };
     samurai2.name = 'Hattori';
     expect(samurai2.name).toBe(__);
-    expect(samurai.name).toBe(__);
+    expect(samurai1.name).toBe(__);
   });
   it('2 - should know how to update object values', function () {
-    var samurai2 = {
-      address: samurai.address
+    var samurai3 = {
+      address: samurai1.address
     };
-    samurai2.address.street = 'Ninja Way';
-    expect(samurai2.address.street).toBe(__);
-    expect(samurai.address.street).toBe(__);
+    samurai3.address.street = 'Ninja Way';
+    expect(samurai3.address.street).toBe(__);
+    expect(samurai1.address.street).toBe(__);
   });
   it('3 - should understand references', function () {
-    samurai.samurai = samurai;
-    expect(samurai.samurai.samurai.samurai.samurai.name).toBe(__);
+    samurai1.samurai = samurai1;
+    expect(samurai1.samurai.samurai.samurai.samurai.name).toBe(__);
   });
 });

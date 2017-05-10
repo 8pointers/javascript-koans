d4be46cc37852722026fad809901fdd1948f5205
Damjan Vujnovic <damjan@samuraiprinciple.com>
2017-05-10 15:15:24 +0100

    update

--- koans/dont-peek/observable/game-of-life.js
+++ koans/dont-peek/observable/game-of-life.js
@@ -1,16 +1,47 @@
-(function () {
-  var oldGameOfLife = SAMURAIPRINCIPLE.GameOfLife;
-  SAMURAIPRINCIPLE.GameOfLife = function () {
-    var self = this, oldtoggleCellState;
-    oldGameOfLife.call(this);
-    SAMURAIPRINCIPLE.eventDispatcher(this);
-    oldtoggleCellState = this.toggleCellState;
-    this.toggleCellState = function (row, column) {
-      oldtoggleCellState(row, column);
-      self.dispatchEvent('cellStateChanged', row, column, self.isCellAlive(row, column));
+/*global jQuery*/
+var SAMURAIPRINCIPLE = SAMURAIPRINCIPLE || {};
+SAMURAIPRINCIPLE.GameOfLife = function () {
+  var self = this,
+    isAlive = {},
+    cellKey = function (row, column) {
+      return row + '_' + column;
     };
+  SAMURAIPRINCIPLE.eventDispatcher(this);
+  this.isCellAlive = function (row, column) {
+    return isAlive[cellKey(row, column)] || false;
   };
-}());
+  this.toggleCellState = function (row, column) {
+    var key = cellKey(row, column);
+    if (isAlive[key]) {
+      delete isAlive[key];
+    } else {
+      isAlive[key] = true;
+    }
+    this.dispatchEvent(...);
+    return this;
+  };
+  this.tick = function () {
+    var key, parts, row, column, numberOfNeighbours = {}, neighbourKey;
+    for (key in isAlive) {
+      parts = key.split('_');
+      row = parseInt(parts[0], 10);
+      column = parseInt(parts[1], 10);
+      numberOfNeighbours[key] = numberOfNeighbours[key] || 0;
+      [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]].forEach(function (offset) {
+        neighbourKey = cellKey(row + offset[0], column + offset[1]);
+        numberOfNeighbours[neighbourKey] = (numberOfNeighbours[neighbourKey] || 0) + 1;
+      });
+    }
+    for (key in numberOfNeighbours) {
+      if (isAlive[key] && (numberOfNeighbours[key] < 2 || numberOfNeighbours[key] > 3) || !isAlive[key] && numberOfNeighbours[key] === 3) {
+        parts = key.split('_');
+        row = parseInt(parts[0], 10);
+        column = parseInt(parts[1], 10);
+        self.toggleCellState(row, column);
+      }
+    }
+  };
+};
 jQuery.fn.gameOfLifeWidget = function (gameOfLife, rows, columns, animationDuration) {
   return this.each(function () {
     var self = jQuery(this);

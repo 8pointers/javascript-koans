b534c8030b934ddb472b6b16bb846f9d937c286d
Damjan Vujnovic <damjan@samuraiprinciple.com>
2017-09-27 14:24:44 +0100

    update

--- koans/dont-peek/observable/eventDispatcher.js
+++ koans/dont-peek/observable/eventDispatcher.js
@@ -1,12 +1,12 @@
 var SAMURAIPRINCIPLE = SAMURAIPRINCIPLE || {};
 SAMURAIPRINCIPLE.eventDispatcher = function (base) {
-  var listeners = [];
+  const listeners = [];
   base.addEventListener = function (type, listener, priority) {
     if (!listener) {
       listener = type;
       type = 'default';
     }
-    listeners.push({ type, listener, priority});
+    listeners.push({type, listener, priority});
   };
   base.listener = function () {
     return listeners[0].listener;
@@ -16,25 +16,25 @@ SAMURAIPRINCIPLE.eventDispatcher = function (base) {
       args = [eventType];
       eventType = 'default';
     }
-    listeners.filter(({type}) => type === eventType)
+    listeners
+      .filter(({type}) => type === eventType)
       .sort(({priority: priorityOne}, {priority: priorityTwo}) => priorityTwo - priorityOne)
       .some(({listener}) => {
         try {
           return listener.apply(undefined, args) === false;
         } catch (error) {
+          console.log('Error dispatching event', error);
         }
       });
   };
   base.createObservableProperty = function (propertyName) {
-    var propertyValue;
-    base['on' + propertyName + 'Changed'] = base.addEventListener.bind(base, propertyName + 'Changed');
-    base['set' + propertyName] = function (value) {
+    let propertyValue;
+    base[`on${propertyName}Changed`] = base.addEventListener.bind(base, `${propertyName}Changed`);
+    base[`set${propertyName}`] = function (value) {
       propertyValue = value;
-      base.dispatchEvent(propertyName + 'Changed', value);
+      base.dispatchEvent(`${propertyName}Changed`, value);
     };
-    base['get' + propertyName] = function () {
-      return propertyValue;
-    };
+    base[`get${propertyName}`] = () => propertyValue;
   };
   return base;
 };

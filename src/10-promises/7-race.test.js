c100fc53d3a1ed1a5496758193b6bca30f990367
Damjan Vujnovic <damjan@samuraiprinciple.com>
2018-05-20 23:05:01 +0100

    update

--- src/10-promises/7-race.test.js
+++ src/10-promises/7-race.test.js
@@ -1,17 +1,19 @@
-describe('Promise.race', function () {
-  const delay = timeInMillis => new Promise(resolve => setTimeout(resolve, timeInMillis));
-  test('should understand Promise.race', function () {
+describe('Promise.race', function() {
+  const delay = timeInMillis =>
+    new Promise(resolve => setTimeout(resolve, timeInMillis));
+  test('should understand Promise.race', function() {
     return Promise.race([
       delay(200).then(() => 'first'),
       delay(100).then(() => 'second')
     ]).then(result => expect(result).toBe(__));
   });
-  test('should understand Promise.race', function () {
+  test('should understand Promise.race', function() {
     return Promise.race([
       delay(100).then(() => 'first'),
       delay(200).then(() => 'second'),
       delay(50).then(() => Promise.reject('third'))
-    ]).then(() => console.log('This should be reached'))
+    ])
+      .then(() => console.log('This should be reached'))
       .catch(reason => expect(reason).toEqual(__));
   });
 });

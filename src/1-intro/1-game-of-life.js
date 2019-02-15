5256979d55f165ac418c8ad58972d3c86a4917f7
Damjan Vujnovic <damjan@samuraiprinciple.com>
2019-02-15 20:31:49 +0100

    update

--- src/1-intro/1-game-of-life.js
+++ src/1-intro/1-game-of-life.js
@@ -1,5 +1,5 @@
 function isCellAliveInNextGeneration(isCellAlive, numberOfLiveNeighbours) {
-  //TODO - Implement one test at a time
+  //TODO - Implement, one test at a time
 }
 
 export default isCellAliveInNextGeneration;
--- src/2-objects/9-game-of-life.js
+++ src/2-objects/9-game-of-life.js
@@ -1,5 +1,10 @@
-function isCellAliveInNextGeneration(isCellAlive, numberOfLiveNeighbours) {
-  //TODO - Implement one test at a time
-}
+/*
+  TODO - Implement, one test at a time.
+  However, you are not allowed to use:
+    - conditional statements (like if/else/switch) or operators (?:)
+    - comparison operators (like ===, !==, <, >, <=, >=, ...)
+    - logical operators (like &&, ||, !, ...)
+  */
+function isCellAliveInNextGeneration(isCellAlive, numberOfLiveNeighbours) {}
 
 export default isCellAliveInNextGeneration;

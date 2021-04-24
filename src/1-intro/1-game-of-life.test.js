2824d54902e662c8d6b2ea0786191ae0f9f66937
Damjan Vujnovic <damjan@8pointers.com>
2021-04-24 18:58:57 +0100

    update

--- src/1-intro/1-game-of-life.test.js
+++ src/1-intro/1-game-of-life.test.js
@@ -1,14 +1,14 @@
 import isCellAliveInNextGeneration from './1-game-of-life';
 
-test('should return false when a live cell has fewer than two live neighbours - under-population', function() {
+test('should return false when a live cell has fewer than two live neighbours - under-population', function () {
   //TODO - write a failing test representative of this requirement
 });
-test('should return true when a live cell has two or three live neighbours - survival', function() {
+test('should return true when a live cell has two or three live neighbours - survival', function () {
   //TODO - write a failing test representative of this requirement
 });
-test('should return false when a live cell has more than three live neighbours - overcrowding', function() {
+test('should return false when a live cell has more than three live neighbours - overcrowding', function () {
   //TODO - write a failing test representative of this requirement
 });
-test('should return true when a dead cell has exactly three live neighbours - reproduction', function() {
+test('should return true when a dead cell has exactly three live neighbours - reproduction', function () {
   //TODO - write a failing test representative of this requirement
 });

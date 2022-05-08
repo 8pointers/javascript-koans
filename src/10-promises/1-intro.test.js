b34d9c5015d4e9b25aadac51c6d5392ef3f2e8bb
Damjan Vujnovic <damjan@8pointers.com>
2022-05-08 08:39:41 +0100

    update

--- src/10-promises/1-intro.test.js
+++ src/10-promises/1-intro.test.js
@@ -1,13 +1,10 @@
 import serve from './serve';
 
-describe('Promises', function () {
+describe('Promises', () => {
   const port = serve(3001);
   const get = (url) => fetch(`http://localhost:${port}/${url}`).then((response) => response.json());
 
-  test('1 - should understand then', function () {
-    return get('data/leaderboard.json').then((l) => expect(l).toEqual(__));
-  });
-  test('2 - should understand then', function () {
-    return get('data/player/1.json').then((p) => expect(p).toEqual(__));
-  });
+  test('1 - should understand then', () =>
+    get('data/leaderboard.json').then((leaderboard) => expect(leaderboard).toEqual(__)));
+  test('2 - should understand then', () => get('data/player/1.json').then((player) => expect(player).toEqual(__)));
 });
